3f5c7bdba61dcedf6e05c73424251cfa63b79a36
haleema786pk <86046927+haleema786pk@users.noreply.github.com>
2023-09-15 12:49:29 +0500

    NewBranch

--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -45,6 +45,7 @@ const Stopwatch: React.FC = () => {
   };
 
   const formatTime = () => {
+    //Function to formate time 
     const getSeconds = `0${(timer % 60)}`.slice(-2);
     const minutes = Number(`${Math.floor(timer / 60)}`);
     const getMinutes = `0${minutes % 60}`.slice(-2);
